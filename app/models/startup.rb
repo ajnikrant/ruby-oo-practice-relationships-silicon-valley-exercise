89d51f6ca5b29b349e35e1c30b7b4877149b8d27
Amanda <ajnikrant@gmail.com>
2021-02-26 17:11:32 -0500

    commit tree fa787354cee80e0d09d4b989c7571bb5812f7e31

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Startup
+    @@all = []
+
+    attr_reader :founder
+    attr_accessor :name, :domain
+
+    def initialize (name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+
+        @@all << self
+    end
+
+    def pivot(domain_new, name_new)
+        @domain = domain_new
+        @name = name_new
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder (name)
+        Startup.all.find{|name| startup.name = name}
+        binding.pry
+    end
+
 
 end
